7946702f1acd34f5e13e97dcea83aa97eb761442
Antonio Gallimore <antonio.gallimore@gmail.com>
2019-10-02 14:19:56 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,35 @@
+class Song
+  attr_accessor :artist, :genre, :name
+  @@artist = 0 
+  @@genre = 0 
+  @@name = 0 
+  @@count = 0 
+  # def initialize
+   # @@artist += 1
+  #end
+  #def initialize
+   # @@genre += 1
+  #end
+# def initialize
+   # @@name += 1
+ # end
+ 
+ def initialize
+   @@count
+ end
+ 
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+  end
+  
+ def song=(song)
+   @song = (song)
+   
+ end 
+ 
+ def song
+   @song 
+ end 
+end
